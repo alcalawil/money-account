ca0e4ae8d2eeae34cb17aadbba9b199fe7a58b0b
Wilfredo Alcala <alcalawilfre@gmail.com>
2020-04-01 22:00:31 -0300

    fix api port

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT) || 5000;
 const app = require("./app");
 const server = http.createServer(app);
 
